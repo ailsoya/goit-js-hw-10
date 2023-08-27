d6098c30a2665bea1e53aee9f657163187a4a7f7
ailsoya <akalitata53@gmail.com>
2023-08-27 15:10:38 +0300

    fixin

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,37 @@
 import axios from "axios"
+const selector = document.querySelector('.breed-select')
+const infoblock = document.querySelector('.cat-info')
 
 function fetchBreeds() {
     const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
 
     return axios
         .get(BASE_URL)
-        .then(response => response.data)
-        .catch(err => error(err))
+        .then(function (response) {
+            const markup = response.data
+                .map((responsi) => {
+                    return `<option value="${responsi.id}">${responsi.name}</option>`
+                })
+                .join("")
+                selector.innerHTML = markup
+                return response.data
+        })
+        .catch(err => console.log(err))
 }
 
-export { fetchBreeds }
+function fetchCatByBreed(breedId) {
+    let fullURL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    return axios
+        .get(fullURL)
+        .then(function (response) {
+            const markup = `<img src="${response.url}">
+                            <h2>${response[0].breeds[0].name}</h2>`
+            infoblock.innerHTML = markup
+        })
+        .catch(err => console.log(err))
+}
+
+fetchBreeds()
+selector.addEventListener('change', function() {
+    fetchCatByBreed(this.value)
+})
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-import { fetchBreeds } from './cat-api'
 
 const selector = document.querySelector('.breed-select')
 axios.defaults.headers.common["x-api-key"] = "live_3L7cmA1jlPkmLCXojWnqLheo3EXPT3mir7WVPIJflud3GvXiLa4XjpTB9jFNiGPR"

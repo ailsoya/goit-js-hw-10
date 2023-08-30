2195f9444344d0ccb391d6a705032fc3ea8a1f2d
ailsoya <akalitata53@gmail.com>
2023-08-30 17:37:32 +0300

    final

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,9 +1,13 @@
 import axios from "axios"
+import Notiflix from 'notiflix'
+const selector = document.querySelector("#selectElement")
 const infoblock = document.querySelector('.cat-info')
-const selector = document.querySelector('.breed-select')
+const loader = document.querySelector('.loader')
 
 function fetchBreeds() {
     const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
+    selector.style.display = 'none'
+    loader.style.display = 'block'
 
     return axios
         .get(BASE_URL)
@@ -14,24 +18,35 @@ function fetchBreeds() {
                 })
                 .join("")
                 selector.innerHTML = markup
+                loader.style.display = 'none'
+                selector.style.display = 'block'
                 return response.data
         })
-        .catch(err => console.log(err))
+        .catch((err) => {
+            loader.style.display = 'none'
+            selector.style.display = 'block'
+            Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`)
+        })
 }
 
 function fetchCatByBreed(breedId) {
     let fullURL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    loader.style.display = 'block'
+    
     return axios
         .get(fullURL)
         .then(function (response) {
             const markup = `<img src="${response.data[0].url}">
                             <div class="text-part"><h2>${response.data[0].breeds[0].name}</h2>
-                            <p>${response.data[0].breeds[0].description}</p>
-                            <span><b>Temperament: </b>${response.data[0].breeds[0].temperament}</span></div>`
+                            <p class="descr">${response.data[0].breeds[0].description}</p>
+                            <span class="temp"><b>Temperament: </b>${response.data[0].breeds[0].temperament}</span></div>`
             infoblock.innerHTML = markup
-            console.log(response.data[0].breeds[0])
+            loader.style.display = 'none'
         })
-        .catch(err => console.log(err))
+        .catch((err) => {
+            Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`)
+            loader.style.display = 'none'
+        })
 }
 
 export { fetchBreeds, fetchCatByBreed }
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js'
-console.log('pooop')
 
 const infoblock = document.querySelector('.cat-info')
-const selector = document.querySelector('.breed-select')
+const selector = document.querySelector("#selectElement")
 axios.defaults.headers.common["x-api-key"] = "live_3L7cmA1jlPkmLCXojWnqLheo3EXPT3mir7WVPIJflud3GvXiLa4XjpTB9jFNiGPR"
+  
 
 fetchBreeds()
 selector.addEventListener('change', function() {

452ade4b3852962620eea72ac049fe80bffbf3d0
ailsoya <akalitata53@gmail.com>
2023-08-31 19:09:28 +0300

    reworkin

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,55 +1,25 @@
 import axios from "axios"
-import Notiflix from 'notiflix'
-const selector = document.querySelector("#selectElement")
-const infoblock = document.querySelector('.cat-info')
-const loader = document.querySelector('.loader')
 
 function fetchBreeds() {
     const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
-    selector.style.display = 'none'
-    loader.style.display = 'block'
 
-    return axios
-        .get(BASE_URL)
-        .then(function (response) {
-            const markup = response.data
-                .map((responsi) => {
-                    return `<option value="${responsi.id}">${responsi.name}</option>`
-                })
-                .join("")
-                selector.innerHTML = markup
-                loader.style.display = 'none'
-                selector.style.display = 'block'
-                return response.data
-        })
-        .catch((err) => {
-            loader.style.display = 'none'
-            selector.style.display = 'block'
-            Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`)
-        })
+    return axios.get(BASE_URL).then(resp => {
+        if(resp.ok == false) {
+            throw new Error(resp.status)
+        }
+        return resp
+    })
 }
 
 function fetchCatByBreed(breedId) {
     let fullURL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    infoblock.style.display = 'none'
-    loader.style.display = 'block'
-    
-    return axios
-        .get(fullURL)
-        .then(function (response) {
-            const markup = `<img src="${response.data[0].url}">
-                            <div class="text-part"><h2>${response.data[0].breeds[0].name}</h2>
-                            <p class="descr">${response.data[0].breeds[0].description}</p>
-                            <span class="temp"><b>Temperament: </b>${response.data[0].breeds[0].temperament}</span></div>`
-            infoblock.innerHTML = markup
-            loader.style.display = 'none'
-            infoblock.style.display = 'flex'
-        })
-        .catch((err) => {
-            Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`)
-            loader.style.display = 'none'
-            infoblock.style.display = 'flex'
-        })
+
+    return axios.get(fullURL).then(resp => {
+        if(resp.ok === false) {
+            throw new Error(resp.status)
+        }
+        return resp
+    })
 }
 
 export { fetchBreeds, fetchCatByBreed }
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,62 @@
 import axios from "axios"
+import { Notify } from "notiflix"
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js'
+axios.defaults.headers.common["x-api-key"] = "live_3L7cmA1jlPkmLCXojWnqLheo3EXPT3mir7WVPIJflud3GvXiLa4XjpTB9jFNiGPR" 
 
-const selector = document.querySelector("#selectElement")
-axios.defaults.headers.common["x-api-key"] = "live_3L7cmA1jlPkmLCXojWnqLheo3EXPT3mir7WVPIJflud3GvXiLa4XjpTB9jFNiGPR"
-  
+const select = document.querySelector("#single")
+const infoblock = document.querySelector('.cat-info')
+const loader = document.querySelector('.loader')
 
-fetchBreeds()
-selector.addEventListener('change', function() {
-    fetchCatByBreed(this.value)
-})
+fetchBreedsRender()
+select.addEventListener('change', function() {
+    fetchCatByBreedRender(this.value)
+})  
+
+function renderSelect(breeds) {
+    const markup = breeds
+        .map((breed) => {
+            return `<option value="${breed.id}">${breed.name}</option>`
+        })
+        .join("")
+    select.innerHTML = markup
+}
+
+function fetchBreedsRender() {
+    select.style.display = 'none'
+    loader.style.display = 'block'
+    fetchBreeds()
+        .then(breeds => renderSelect(breeds.data))
+        .catch(error => {
+            console.log(error)
+            Notify.failure('Oops! Something went wrong! Try reloading the page!')
+        })
+        .finally(() => {
+            loader.style.display = 'none'
+            select.style.display = 'block'
+        })
+}
+
+function renderCatDescription(breed) {
+    const markup = `<img src="${breed.url}">
+                    <div class="text-part"><h2>${breed.breeds[0].name}</h2>
+                    <p class="descr">${breed.breeds[0].description}</p>
+                    <span class="temp"><b>Temperament: </b>${breed.breeds[0].temperament}</span></div>`
+    infoblock.innerHTML = markup
+}
+
+function fetchCatByBreedRender(value) {
+    infoblock.style.display = 'none'
+    loader.style.display = 'block'
+    fetchCatByBreed(value)
+        .then(breeds => {
+            renderCatDescription(breeds.data[0])
+            loader.style.display = 'none'
+            infoblock.style.display = 'flex'
+        })
+        .catch(error => {
+            console.log(error)
+            Notify.failure('Oops! Something went wrong! Try reloading the page!')
+            loader.style.display = 'none'
+            infoblock.style.display = 'none'
+        })
+}
